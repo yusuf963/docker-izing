3152d199e1ab481e7878220514dc8d988eab4331
yusuf963 <mohammad963yusuf@gmail.com>
2021-04-12 22:47:11 +0100

    adding port enviroment for server to run

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 
 const app = express()
-app.get('/', (req, res) => res.send('My app is running!'))
+app.get('/', (req, res) => res.send('this  server is up and running 🚀🚀🚀. as I have managed to build a webhook to catch any change in my server and auto deploy(continuous deployment) this is v2'))
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
   console.log('my app is running on port 3000')
 })
